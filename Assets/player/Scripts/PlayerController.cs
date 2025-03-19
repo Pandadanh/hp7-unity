8bb97a371604901269b09f11bd3aa203d3039701
Benedict1210 <167989768+Benedict1210@users.noreply.github.com>
2025-03-19 20:18:42 +0700

    Apply stash and overwrite local changes

--- Assets/player/Scripts/PlayerController.cs
+++ Assets/player/Scripts/PlayerController.cs
@@ -4,31 +4,59 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 5f, jumpForce = 15f;
+    [SerializeField] private float moveSpeed = 5f, jumpForce = 15f, projectileSpeed = 10f, projectileCD = .5f;
     [SerializeField] private int JumpCount = 2;
+    [SerializeField] private GameObject projectilePrefab;
     [SerializeField] private LayerMask groundLayer;
-    [SerializeField] private Transform groundCheck;
+    [SerializeField] private Transform groundCheck, shootingPoint;
 
-    //private Animator animator;
+    private Animator animator;
     private bool isGrounded;
-    private float jumps;
+    private float jumps, lastShot = 0;
     private Rigidbody2D rb;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        //animator = GetComponent<Animator>();
+        animator = GetComponent<Animator>();
     }
     void Start()
     {
-        
+        lastShot = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
+        HandleShoot();
         HandleMovement();
         HandleJump();
-        //UpdateAnimation();
+        UpdateAnimation();
+    }
+    private void HandleShoot()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+
+        if (isGrounded)
+        {
+            jumps = JumpCount;
+        }
+
+        if (Input.GetButtonDown("Fire1") & (Time.time - lastShot) >= projectileCD)
+        {
+            GameObject projectile = Instantiate(projectilePrefab, shootingPoint.position, shootingPoint.rotation);
+
+            // Apply forward velocity to the projectile
+            Rigidbody rb = projectile.GetComponent<Rigidbody>();
+
+            print(shootingPoint);
+
+            if (rb != null)
+            {
+                rb.linearVelocity = shootingPoint.forward * projectileSpeed;
+            }
+
+            lastShot = Time.time;
+        }
     }
     private void HandleMovement()
     {
@@ -66,8 +94,10 @@ public class PlayerController : MonoBehaviour
     {
         bool isRunning = Mathf.Abs(rb.linearVelocity.x) > .1f;
         bool isJumping = !isGrounded;
+        bool isAiming = Input.GetButtonDown("Fire1");
 
-        //animator.SetBool("isRunning", isRunning);
-        //animator.SetBool("isJumping", isJumping);
+        animator.SetBool("Walking", isRunning);
+        animator.SetBool("Jumping", isJumping);
+        animator.SetBool("Aiming", isAiming);
     }
 }
